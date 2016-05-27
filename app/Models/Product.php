97f49ef6ec9ded89af491b537e501c89c00e3eec
Pedro Felipe <pedrinhof11@gmail.com>
2016-05-27 20:36:52 -0300

    Primeiro commit

---
+++ app/Models/Product.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace CodeDelivery\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'category_id',
+        'name',
+        'description',
+        'price'
+    ];
+}
